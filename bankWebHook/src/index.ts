bcd70254767e121e8e81fe9d8788fd12167fdb0d
Kartik Kaul <kartik3322@gmail.com>
2025-05-27 09:30:25 +0530

    removed_dotenv

--- bankWebHook/src/index.ts
+++ bankWebHook/src/index.ts
@@ -1,8 +1,3 @@
-import path from "path"
-import dotenv from "dotenv";
-dotenv.config({
-  path: path.resolve(__dirname, "../../.env")
-});
 import express from 'express';
 import db from '../../prisma/db';
 import { z } from 'zod';
